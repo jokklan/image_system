6449f19dfa51b07a9e5aa01d8d4f769827aa43ad
andreorvalho <andre.orvalho@getgears.com>
2013-12-04 11:30:03 +0100

    added test to upload an image if saved or changed uuid

--- lib/image_system/image.rb
+++ lib/image_system/image.rb
@@ -1,11 +1,18 @@
 require 'cdn/communication_system'
+require 'exceptions/cdn_upload_exception'
 
 module ImageSystem
   module Image
 
     def save
-      CDN::CommunicationSystem.upload(uuid: self.uuid, source_file_path: self.path, queue_processing: false)
-      super
+      begin
+        if self.new_record? || self.changed.include?("uuid")
+          CDN::CommunicationSystem.upload(uuid: self.uuid, source_file_path: self.path, queue_processing: false)
+        end
+        super
+      rescue Exceptions::CdnUploadException => e
+        return false
+      end
     end
 
   end
--- spec/lib/image_system/image_spec.rb
+++ spec/lib/image_system/image_spec.rb
@@ -4,12 +4,13 @@ require 'cdn/communication_system'
 require 'image_system'
 require 'support/schema'
 require 'uuidtools'
+require 'exceptions/cdn_upload_exception'
 
 describe "Photo" do
 
   describe "#save" do
 
-    it "should only save if the image as been uploaded successfully" do
+    it "saves an image that is new and has been uploaded successfully" do
       p = Photo.new(uuid: UUIDTools::UUID.random_create.to_s.gsub(/\-/, ''), path: "#{Rails.root}/public/images/test_image.jpg")
       p.should be_new_record
 
@@ -19,6 +20,38 @@ describe "Photo" do
       p.save
       p.should_not be_new_record
     end
+
+    it "does not save an image that is new and has not been uploaded successfully" do
+      p = Photo.new(uuid: UUIDTools::UUID.random_create.to_s.gsub(/\-/, ''), path: "#{Rails.root}/public/images/test_image.jpg")
+      p.should be_new_record
+
+      CDN::CommunicationSystem.stub(:upload).and_raise(Exceptions::CdnUploadException.new("failed to upload"))
+      CDN::CommunicationSystem.should_receive(:upload)
+
+      p.save
+      p.should be_new_record
+    end
+
+    it "does not upload a new image that is not a new record" do
+      CDN::CommunicationSystem.stub(:upload) { true }
+      p = Photo.create(uuid: UUIDTools::UUID.random_create.to_s.gsub(/\-/, ''), path: "#{Rails.root}/public/images/test_image.jpg")
+      p.should_not be_new_record
+
+      CDN::CommunicationSystem.should_not_receive(:upload)
+      p.save
+    end
+
+    it "uploads the image if its not a new record but has a new uuid" do
+      CDN::CommunicationSystem.stub(:upload) { true }
+      p = Photo.create(uuid: UUIDTools::UUID.random_create.to_s.gsub(/\-/, ''), path: "#{Rails.root}/public/images/test_image.jpg")
+      p.should_not be_new_record
+
+      CDN::CommunicationSystem.should_receive(:upload)
+      p.uuid = "new_uuid"
+      p.save
+
+    end
+
   end
 
 end
